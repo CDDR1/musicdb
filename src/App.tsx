a6683a46e72fcfc493978f12ce7ce61a73c77d6a
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-08-29 11:04:40 -0400

    Adding React Router and setting up the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Search from "./components/Search";
-import Tracks from "./components/Tracks";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import TrackDetails from "./pages/TrackDetails";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  const [songs, setSongs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  
-  const getData = async (e: any) => {
-    e.preventDefault();
-
-    setLoading(true);
-
-    try {
-      const res: any = await fetch(`https://itunes.apple.com/search?term=${searchTerm}&resultEntity=music&limit=18`);
-      const data: any = await res.json();
-      setSongs(data.results);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      return <div>There has been an error fetching the data </div>
-    }
-  };
-
-  const handleInputChange = (text: string) => {
-    setSearchTerm(text);
-  };
-
   return (
-    <div className="container">
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Search handleSearch={handleInputChange} fetchData={getData} />
-        {loading ? <h1>Loading...</h1> : <Tracks tracks={songs} />}
-      </main>
-      <footer></footer>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/track/:id" element={<TrackDetails />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,44 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Search from "../components/Search";
+import Tracks from "../components/Tracks";
+
 const Home = () => {
+  const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  const getData = async (e: any) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    try {
+      const res: any = await fetch(`https://itunes.apple.com/search?term=${searchTerm}&resultEntity=music&limit=18`);
+      const data: any = await res.json();
+      setSongs(data.results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      return <div>There has been an error fetching the data </div>
+    }
+  };
+
+  const handleInputChange = (text: string) => {
+    setSearchTerm(text);
+  };
+
   return (
-    <div></div>
+    <div className="container">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Search handleSearch={handleInputChange} fetchData={getData} />
+        {loading ? <h1>Loading...</h1> : <Tracks tracks={songs} />}
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
